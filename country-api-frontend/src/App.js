7a178a19570426365d89c9b3d0a18471754c6443
Jayaisuru Samarakoon <128087293+IT22569486@users.noreply.github.com>
2025-04-11 01:25:22 +0530

    3rd commit
    Added filter and search functionalities.

--- country-api-frontend/src/App.js
+++ country-api-frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import CountryList from './components/CountryList';
+import SearchBar from './components/SearchBar';
+import Filter from './components/Filter';
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -7,13 +9,17 @@ function App() {
   useEffect(() => {
     fetch('https://restcountries.com/v3.1/all')
       .then((response) => response.json())
-      .then((data) => setCountries(data))
-      .catch((error) => console.error('Error fetching countries:', error));
+      .then((data) => setCountries(data));
   }, []);
+
+  const handleSearch = (data) => setCountries(data);
+  const handleFilter = (data) => setCountries(data);
 
   return (
     <div className="container">
       <h1>REST Countries Explorer</h1>
+      <SearchBar onSearch={handleSearch} />
+      <Filter onFilter={handleFilter} />
       <CountryList countries={countries} />
     </div>
   );
